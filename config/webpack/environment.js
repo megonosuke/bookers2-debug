877c46807764a2d164bb74b73952506ca994a7a0
MegumiKamata <141466592+megonosuke@users.noreply.github.com>
2023-09-14 05:12:18 +0000

    [Rest]

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,5 @@
 const { environment } = require('@rails/webpacker')
+module.exports = environment
 
 const webpack = require('webpack')
 environment.plugins.prepend(
@@ -10,4 +11,3 @@ environment.plugins.prepend(
   })
 )
 
-module.exports = environment
