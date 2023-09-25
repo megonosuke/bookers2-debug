28df04f42bc58314e863cf0decba225d096f9cd0
MegumiKamata <141466592+megonosuke@users.noreply.github.com>
2023-09-25 07:17:33 +0000

    [Fix]

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,7 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :followings, through: :relationships, source: :followed
-
+  
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 
--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,6 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
-  resources :users, only: [:index,:show,:edit,:update] do
-    resource :relationships, only: [:create, :destroy]
-  end
-
+  resources :users, only: [:index,:show,:edit,:update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
