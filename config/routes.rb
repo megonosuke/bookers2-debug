d95a31d1754ce33844dd537e8d7f7ccc0e4a1d83
MegumiKamata <141466592+megonosuke@users.noreply.github.com>
2023-09-26 02:32:32 +0000

    [Rest]

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   get 'relationships/followings'
   get 'relationships/followers'
-  get "search" => "searches#search"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "homes#top"
   get "home/about"=>"homes#about"
+  
   devise_for :users
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only: [:create, :destroy]
@@ -16,5 +17,7 @@ Rails.application.routes.draw do
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
+  
+  get "/search", to: "searches#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
